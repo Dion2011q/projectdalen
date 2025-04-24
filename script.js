00a9d7766ec83ca0a3a03ed8ff27a24f5a861a23
Dion Dogger <diondogger@Macbook-Air-van-Dion.local>
2025-04-24 19:42:25 +0200

    de basis van script en de stylesheet

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleTheme() {
+    const currentTheme = document.body.getAttribute('data-theme');
+    document.body.setAttribute('data-theme', currentTheme === 'dark' ? 'light' : 'dark');
+}
